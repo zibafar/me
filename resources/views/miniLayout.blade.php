0ceb1be41ddc1e6980c44bd333e3cc26e5a14762
Sadjad Zibafar <sadjadzibafar@gmail.com>
2019-01-19 12:15:36 +0330

    create prj

---
+++ resources/views/miniLayout.blade.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="{{asset('/')}}css/main.css">
+    <meta name="developer" content="sadjad zibafar">
+    <meta name="website" content="www.zibafar.ir">
+    <meta name='copyright' content='Gilace'>
+    <meta name='designer' content='www.Gilace.com'>
+    <meta name='reply-to' content='sadjadzibafar@gmail.com'>
+
+    @yield('css')
+    <title>ورود</title>
+</head>
+<body>
+
+<div class="my_container">
+    @yield('content')
+</div>
+@yield('js')
+</body>
+</html>
